2708e9559c062f6edc5a4ecb31ed327f422f6091
vishal7732 <58137166+vishal7732@users.noreply.github.com>
2020-04-25 01:27:01 +0530

    Update settings.py

--- blogs/settings.py
+++ blogs/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'blogs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': ['vishal7732/blog/tree/master/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
